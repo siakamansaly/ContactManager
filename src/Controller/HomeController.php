b63f4c575b6ae4808b18055106eafe4c1a0f0428
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-09-24 00:10:54 +0200

    config login page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
+     * @IsGranted("ROLE_USER")
      */
     public function index(): Response
     {
